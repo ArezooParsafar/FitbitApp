f8fd39adefc19001fd46194101f4b1ba3aa8486f
ArezooParsafar <arezoo.parsafar86@gmail.com>
2021-10-04 15:15:18 +0100

    Add project files.

---
+++ FitbitApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Fitbit.Api.Portable;
+using FitbitApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FitbitApp.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public HomeController()
+        {
+            var cred=""
+        }
+
+        public ActionResult Index(CredModel model)
+        {
+            if(model == null)
+            {
+                return View();
+            }
+
+            return View("downloadfile", model);
+        }
+    }
+}
